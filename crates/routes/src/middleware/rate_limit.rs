906fd5dab030a328dd68fd41979234bddd5e79ce
Felix Ableitner <me@nutomic.com>
2025-06-03 12:22:44 +0200

    wip

--- crates/routes/src/middleware/rate_limit.rs
+++ crates/routes/src/middleware/rate_limit.rs
@@ -1,18 +1,31 @@
 use actix_extensible_rate_limit::{
-  backend::{memory::InMemoryBackend, SimpleInputFunctionBuilder, SimpleInputFuture, SimpleOutput},
+  backend::{memory::InMemoryBackend, Backend, Decision, SimpleOutput},
   RateLimiter,
 };
 use actix_web::dev::ServiceRequest;
-use lemmy_db_schema::source::local_site_rate_limit::LocalSiteRateLimit;
-use std::time::Duration;
+use enum_map::EnumMap;
+use lemmy_utils::rate_limit::{ActionType, BucketConfig};
+use std::{
+  convert::Infallible,
+  future::{ready, Ready},
+  sync::{Arc, Mutex},
+};
+
+type RateLimitConfig = Arc<Mutex<EnumMap<ActionType, BucketConfig>>>;
 
 pub struct RateLimit {
-  limits: LocalSiteRateLimit,
+  limits: RateLimitConfig,
   message: InMemoryBackend,
 }
 
+pub struct LemmyInput {
+  limits: RateLimitConfig,
+}
+
+pub type LemmyInputFuture = Ready<Result<LemmyInput, actix_web::Error>>;
+
 impl RateLimit {
-  pub fn new(limits: LocalSiteRateLimit) -> Self {
+  pub fn new(limits: RateLimitConfig) -> Self {
     Self {
       limits,
       message: InMemoryBackend::builder().build(),
@@ -20,13 +33,12 @@ impl RateLimit {
   }
 
   fn build_rate_limiter(
-    backend: &InMemoryBackend,
-    interval: Duration,
-    mut max_requests: u64,
+    &self,
+    action_type: ActionType,
   ) -> RateLimiter<
     InMemoryBackend,
     SimpleOutput,
-    impl Fn(&ServiceRequest) -> SimpleInputFuture + 'static,
+    impl Fn(&ServiceRequest) -> LemmyInputFuture + 'static,
   > {
     // TODO these have to be set, because the database defaults are too low for the federation
     // tests to pass, and there's no way to live update the rate limits without restarting the
@@ -35,9 +47,23 @@ impl RateLimit {
     if cfg!(debug_assertions) {
       max_requests = 999;
     }
-    let input = SimpleInputFunctionBuilder::new(interval, max_requests)
-      .real_ip_key()
-      .build();
+
+    let input = move |req: &ServiceRequest| {
+      ready((|| {
+        let mut components: Vec<String> = Vec::new();
+        let info = req.connection_info();
+        // https://github.com/jacob-pro/actix-extensible-rate-limit/blob/master/src/backend/input_builder.rs#L139
+        components.push(todo!());
+
+        let key = components.join("-");
+
+        Ok(LemmyInput {
+          limits: self.limits,
+        })
+      })())
+    };
+    // TODO: use different backend depending on action_type
+    let backend = self.message;
     RateLimiter::builder(backend.clone(), input)
       .add_headers()
       .rollback_server_errors()
@@ -49,13 +75,31 @@ impl RateLimit {
   ) -> RateLimiter<
     InMemoryBackend,
     SimpleOutput,
-    impl Fn(&ServiceRequest) -> SimpleInputFuture + 'static,
+    impl Fn(&ServiceRequest) -> LemmyInputFuture + 'static,
   > {
-    Self::build_rate_limiter(
-      &self.message,
-      Duration::from_secs(self.limits.message.try_into().unwrap_or(0)),
-      // TODO: rename db fields as message_per_interval
-      self.limits.message_per_second.try_into().unwrap_or(0),
-    )
+    self.build_rate_limiter(ActionType::Message)
+  }
+}
+
+// https://github.com/jacob-pro/actix-extensible-rate-limit/blob/master/src/backend/memory.rs#L70
+impl Backend<LemmyInput> for InMemoryBackend {
+  type Output = SimpleOutput;
+
+  type RollbackToken = String;
+
+  type Error = Infallible;
+
+  async fn request(
+    &self,
+    input: LemmyInput,
+  ) -> Result<(Decision, Self::Output, Self::RollbackToken), Self::Error> {
+    todo!()
+  }
+
+  async fn rollback(&self, token: Self::RollbackToken) -> Result<(), Self::Error> {
+    self.map.entry(token).and_modify(|v| {
+      v.count = v.count.saturating_sub(1);
+    });
+    Ok(())
   }
 }
